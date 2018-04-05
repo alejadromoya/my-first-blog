b10ba0abd8802834ffeb8c81c4bbc501b42bad11
alejandromoya <alejandro@publiaciertos.net>
2018-04-05 17:39:12 -0400

    Django Girls app

---
+++ hello.py
@@ -0,0 +1,15 @@
+def hi(name):
+	print("Hi "+name+"!!!")
+
+def gurls():
+	girls = ["stef","karla","ana","azelb","karol"]
+	for name in girls:
+		hi(name)
+		print("next_gurl =>")
+# gurls()
+
+def fff():
+	for x in range(1,10):
+		print(x)
+fff()
+
